5f8b4268931ae9740add8bc1f02bab4bca431126
tylerfoo04 <103219923+tylerfoo04@users.noreply.github.com>
2023-09-29 11:20:38 -0400

    Add files via upload

---
+++ WordMatcher/FileReader.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WordUnscrambler
+{
+    class FileReader
+    {
+        public string[] Read(string filename)
+        {
+            // Your Code here!
+
+            return null;
+        }
+    }
+}
